98a547aeac08450e4f51f47ed03645da6b2bb25d
mohammed-abadi <modernkille3grow@gmail.com>
2026-05-10 17:17:02 +0300

    database set up

---
+++ main_app/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture_url = models.URLField(max_length=500, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    # The Followers table
+    following = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers", blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+
+#  Content
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    media_url = models.URLField(max_length=500)
+    caption = models.TextField(max_length=500, blank=True)
+    is_reel = models.BooleanField(default=True)
+    view_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    topics = models.ManyToManyField(Topic, blank=True, related_name="posts")
+    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+    favorites = models.ManyToManyField(User, related_name="favorited_posts", blank=True)
+
+    def __str__(self):
+        return f"Post {self.id} by {self.user.username}"
+
+
+class Repost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    original_post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="reposts"
+    )
+    repost_caption = models.TextField(max_length=250, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField(max_length=500)
+    # This handles the 'parent_comment_id' from diagram for threaded replies
+    parent_comment = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+#  Messaging System
+
+
+class Conversation(models.Model):
+    is_group = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Participant(models.Model):
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="participants"
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="messages"
+    )
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    is_read = models.BooleanField(default=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
