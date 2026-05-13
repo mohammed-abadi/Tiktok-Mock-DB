46c3c549e8549c441b5ebf5c1976247db4a18ff3
mohammed-abadi <modernkille3grow@gmail.com>
2026-05-13 03:10:28 +0300

    im in deployment hell i think?

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from django.contrib import admin
-from .models import Profile, Post, Topic, Comment, Conversation, Message, Participant
+from .models import Profile, Post, Topic, Comment, Conversation, Message
 
 admin.site.register(Profile)
 admin.site.register(Post)
